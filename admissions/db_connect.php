217da858a48954507afac55742990c18de641e72
bandirenuka <71758275+bandirenuka@users.noreply.github.com>
2021-05-04 07:07:52 +0530

    credentials updation

--- admissions/db_connect.php
+++ admissions/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set('Asia/Kolkata');
-define("HOST", "45.79.134.238"); // The host you want to connect to.
-define("USER", "msitprogram"); // The database username.
-define("PASSWORD", "v3NXLXC5YjFPjmH7"); // The database password. 
-define("DATABASE", "msitprog_admissions"); // The database name.
+define("HOST", "*******"); // The host you want to connect to.
+define("USER", "*******"); // The database username.
+define("PASSWORD", "*******"); // The database password. 
+define("DATABASE", "*******"); // The database name.
 define("prefix","");
 ini_set('session.bug_compat_warn', 0);
 ini_set('session.bug_compat_42', 0);
